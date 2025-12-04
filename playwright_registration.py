c29c9f5e60353302729acda990f24eed2c9f48bf
Lebedkin Konstantin <lekoev.nvk@gmail.com>
2025-12-05 01:11:25 +0500

    Работа с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -3,20 +3,30 @@ from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
 
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
     email_reg_input = page.get_by_test_id('registration-form-email-input').locator('//div//input')
     email_reg_input.fill('user.name@gmail.com')
 
-    username_registration_input = page.get_by_test_id('registration-form-username-input').locator('//div//input')
-    username_registration_input.fill('username')
+    username_reg_input = page.get_by_test_id('registration-form-username-input').locator('//div//input')
+    username_reg_input.fill('username')
 
-    password_registration_input = page.get_by_test_id('registration-form-password-input').locator('//div//input')
-    password_registration_input.fill('password')
+    password_reg_input = page.get_by_test_id('registration-form-password-input').locator('//div//input')
+    password_reg_input.fill('password')
 
-    button_registration = page.get_by_test_id('registration-page-registration-button')
-    button_registration.click()
+    reg_button = page.get_by_test_id('registration-page-registration-button')
+    reg_button.click()
 
-    expect(page.get_by_test_id('dashboard-toolbar-title-text')).to_be_visible()
+    context.storage_state(path='browser-state.json')
+
+    # expect(page.get_by_test_id('dashboard-toolbar-title-text')).to_be_visible()
+    # expect(page.get_by_test_id('dashboard-toolbar-title-text')).to_have_text('Dashboard')
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
